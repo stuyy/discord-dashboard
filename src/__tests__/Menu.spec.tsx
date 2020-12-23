564a308d98a1ac03fe00d132cbc244c5e7aec4ba
Anson <ansfoong@gmail.com>
2020-12-23 07:56:45 -0500

    Updated Tests

--- src/__tests__/Menu.spec.tsx
+++ src/__tests__/Menu.spec.tsx
@@ -1,10 +1,19 @@
 import * as React from "react";
 import { render, screen } from "../utils/test";
 import { Menu } from "../components/Menu";
+import { useHistory } from "react-router-dom";
+
+jest.mock("react-router-dom", () => ({
+  useHistory: () => ({
+    push: jest.fn(),
+  }),
+}));
 
 it("should render Menu", () => {
-  render(<Menu title="Drinks Bot Menu" />);
-  const element = screen.getByText("Drinks Bot Menu");
+  const history = useHistory();
+  render(<Menu title="Drinks Bot Menu" history={history} />);
+  const element = screen.getByText("Please Select a Guild");
   expect(element).toBeInTheDocument();
   expect(element).toMatchSnapshot();
 });
+
--- src/__tests__/Sidebar.spec.tsx
+++ src/__tests__/Sidebar.spec.tsx
@@ -1,17 +1,31 @@
 /* eslint-disable jest/no-mocks-import */
 import * as React from "react";
-import { render, screen } from "../utils/test";
+import { render } from "../utils/test";
 import { Sidebar } from "../components/Sidebar";
 import { guilds } from "../__mocks__";
+import { useHistory } from "react-router-dom";
+import { History } from "history";
+
+let history: History;
+
+jest.mock("react-router-dom", () => ({
+  useHistory: () => ({
+    push: jest.fn(),
+  }),
+}));
+
+beforeEach(() => {
+  history = useHistory();
+});
 
 it("should render Sidebar", () => {
-  const { asFragment } = render(<Sidebar guilds={guilds} />);
+  const { asFragment } = render(<Sidebar guilds={guilds} history={history} />);
   const firstRender = asFragment();
   expect(firstRender).toMatchSnapshot();
 });
 
 it("should render sidebar with 16 elements", () => {
-  const { container } = render(<Sidebar guilds={guilds} />);
+  const { container } = render(<Sidebar history={history} guilds={guilds} />);
   const div = container.getElementsByClassName("guild-icon");
   expect(div.length).toBe(16);
 });
